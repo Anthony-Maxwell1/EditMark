1fa95f4247d5a31f06ae53e2c4c97ee079ff64a5
Anthony <64576656+Anthony-Maxwell1@users.noreply.github.com>
2023-12-04 15:35:59 +1100

    Create saveraw.js

---
+++ saveraw.js
@@ -0,0 +1,2 @@
+// To use this, add javascript: on the front of the code and paste into url bar or console.
+document.getElementsByTagName("html")[0].innerHTML = document.getElementsByTagName("pre")[0].innerText;
